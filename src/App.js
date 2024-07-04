b4056b95bed58a9c5f2ec63254ae64171b28f883
Stacy Druart <stacy@StacyDruartMSI>
2024-07-04 11:47:03 +0200

    maj

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef } from 'react';
+
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const taskInputRef = useRef(null);
+
+  const addTask = () => {
+    const taskInput = taskInputRef.current;
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+      setTasks(prevTasks => [
+        ...prevTasks,
+        { id: Date.now(), text: taskText, completed: false }
+      ]);
+      taskInputRef.current.value = '';
+    }
+  };
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>My Todo App</h1>
+      <label htmlFor="taskInput">Add Task</label>
+      <input type="text" id="taskInput" ref={taskInputRef} />
+      <button onClick={addTask}>Add</button>
+
+      <h2>Todos</h2>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTaskCompletion(task.id)}
+            />
+            <span className={task.completed ? 'completed' : ''}>
+              {task.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assurez-vous que le chemin est correct ici
+// import 'materialize-css/dist/css/materialize.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+ 
